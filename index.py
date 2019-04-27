acfe3a1fa67d1bd022f6ac084811b879e83207d3
Sebastian <sebas950408@gmail.com>
2019-04-27 10:42:18 -0500

    tipos de datos y conversiones

--- index.py
+++ index.py
@@ -1,5 +1,55 @@
-# -*- coding: utf-8 -*-
+# cometario de linea
+"""
+comentario de parrafo
+"""
 
-a = 5
+# hola mundo
 
-A = 7
+print('Hello world')
+
+# variables
+
+x = 4   # int
+y = "texto"  # string
+z = 4.5  # float
+a = True  # boolean
+b = False  # boolean
+c = 4 + 5j   # complejo
+
+print(x, y, z, a, b, c)
+
+# conocer tipo de dato
+print(type(b))
+
+# conversiones de tipos de datos
+
+# enteros
+x = int(2)
+print(x)
+x = int(2.8)
+print(x)
+x = int("2")
+print(x)
+
+# Flotantes
+
+x = float(2)
+print(x)
+x = float(2.8)
+print(x)
+x = float("2")
+print(x)
+x = float("2.5")
+print(x)
+
+# Cadenas
+
+x = str(2)
+print(x)
+x = str(2.8)
+print(x)
+x = str("2")
+print(x)
+x = str("2.5")
+print(x)
+
